01174c7d2287069d30a6e66d057e390507030606
samarasimhareddy4512 <mssreddy4512@gmail.com>
2024-09-30 21:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let productContainer = document.querySelector('.product-container');
+
+
+let url = 'https://fakestoreapi.com/products';
+
+function loadProducts() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => displayProducts(data))
+        .catch((error) => console.error('Error fetching products:', error));
+}
+
+function displayProducts(products) {
+    productContainer.innerHTML = '';
+
+    products.forEach((product) => {
+        let productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+        
+        let img = document.createElement('img');
+        img.src = product.image;
+        img.alt = product.title;
+        
+        let category = document.createElement('p');
+        category.innerText = Category: ${product.category};
+        
+        let description = document.createElement('p');
+        description.innerText = product.description;
+        
+        let price = document.createElement('p');
+        price.innerText = Price: $${product.price};
+        
+        let rating = document.createElement('p');
+        rating.innerText = Rating: ${product.rating.rate} (${product.rating.count} reviews);
+
+        productDiv.appendChild(img);
+        productDiv.appendChild(category);
+        productDiv.appendChild(description);
+        productDiv.appendChild(price);
+        productDiv.appendChild(rating);
+
+        productContainer.appendChild(productDiv);
+    });
+}
+
+loadProducts();
